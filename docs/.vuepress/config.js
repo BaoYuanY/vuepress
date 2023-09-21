c59aa71e025d2e060a8ef0c889e645a94060a152
baoyuan <yangbaoyuan0304@163.com>
2023-09-21 11:52:30 +0800

    搜索选用algolia

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -8,7 +8,16 @@ module.exports = {
             { text: 'Guide', link: '/guide/' },
             { text: 'External', link: 'https://google.com' },
         ],
-        search: true,
-        searchMaxSuggestions: 10
+        //search: true,
+        //searchMaxSuggestions: 10,
+        algolia: {
+            apiKey: '4819e020eca64964cfb4072be1e0f284',
+            indexName: 'yangbaoyuan',
+            // 如果 Algolia 没有为你提供 `appId` ，使用 `BH4D9OD16A` 或者移除该配置项
+            appId: 'ZBA042F24F',
+            insights: true, // Optional, automatically send insights when user interacts with search results
+            container: '### REPLACE ME WITH A CONTAINER (e.g. div) ###',
+            debug: false // Set debug to true if you want to inspect the modal
+        }
     }
 }
